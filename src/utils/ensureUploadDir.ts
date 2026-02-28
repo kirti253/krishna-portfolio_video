bcc230a9d4d055262e5bfb91ab1396a7b16ba18f
kirti <kirti25032007@gmail.com>
2026-02-28 23:39:36 +0530

    test

--- src/utils/ensureUploadDir.ts
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-import path from "path";
-
-export function ensureUploadDir(dir: string): void {
-  const absolute = path.isAbsolute(dir) ? dir : path.join(process.cwd(), dir);
-  if (!fs.existsSync(absolute)) {
-    fs.mkdirSync(absolute, { recursive: true });
-  }
-}
